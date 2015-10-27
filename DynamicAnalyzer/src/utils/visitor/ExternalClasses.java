5fb8cfa2d10cf59e89c174d2be2106f616ccba5b
Regina Koenig <koenig_regina@arcor.de>
2015-10-27 10:45:08 +0100

    Class for external method calls

---
+++ DynamicAnalyzer/src/utils/visitor/ExternalClasses.java
@@ -0,0 +1,12 @@
+package utils.visitor;
+
+import java.util.HashMap;
+
+public class ExternalClasses {
+
+	protected static HashMap<Class<?>, String> classMap = new HashMap<Class<?>, String>();
+	static {
+		classMap.put(String.class, "asd");
+	}
+	
+}
