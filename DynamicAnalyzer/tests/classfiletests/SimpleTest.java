2264a051a8cb9ce7fbfe96f43d2ea6769bf9de80
Regina Koenig <koenigr@tfpool23.informatik.uni-freiburg.de>
2015-11-12 11:44:12 +0100

    Benchmark doesn't work

--- DynamicAnalyzer/tests/classfiletests/SimpleTest.java
+++ DynamicAnalyzer/tests/classfiletests/SimpleTest.java
@@ -1,22 +1,18 @@
 package classfiletests;
-
-import static org.junit.Assert.*;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
-import org.junit.Test;
+import com.google.caliper.Runner;
+import com.google.caliper.SimpleBenchmark;
 
 import utils.logging.L1Logger;
 
-public class SimpleTest {
+public class SimpleTest extends SimpleBenchmark {
+	
+
 
 	Logger logger = L1Logger.getLogger();
 	
-	@Test
-	public void test() {
+	public void timeTest(int reps) {
 		logger.info("Start of executing main.testclasses.Simple");
 		
 		ClassRunner.runClass("main.testclasses.Simple");
@@ -24,4 +20,24 @@ public class SimpleTest {
 		logger.info("Finished executing main.testclasses.Simple");
 	}
 
+	 
+	 public void timeHashSetAdd(int reps) {
+		// Be sure to add this outer loop.
+		for (int i = 0; i < reps; i++) {
+		// Then, in the inner loop, go ahead and do some meaningful work.
+		// Here we're seeing how long it takes to add this many integers
+		// to a set.
+		for (int j = 0; j < 1000; j++) {
+		 
+		}
+		}
+		}
+		// And now we've got a fight on our hands!
+		public void timeTreeSetAdd(int reps) {
+		for (int i = 0; i < reps; i++) {
+		for (int j = 0; j < 10000; j++) {
+		 
+		}
+		}
+		}
 }
