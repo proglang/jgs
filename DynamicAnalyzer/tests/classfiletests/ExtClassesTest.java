6dea94be85a16f050df257251628eb8dd3c2e885
Regina Koenig <koenig_regina@arcor.de>
2015-11-25 19:38:17 +0100

    added more tests. Changed a littble bit in the classLoader

--- DynamicAnalyzer/tests/classfiletests/ExtClassesTest.java
+++ DynamicAnalyzer/tests/classfiletests/ExtClassesTest.java
@@ -1,14 +1,22 @@
 package classfiletests;
 
-import static org.junit.Assert.*;
 
 import org.junit.Test;
+import utils.logging.L1Logger;
+
+import java.util.logging.Logger;
+
 
 public class ExtClassesTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+  Logger logger = L1Logger.getLogger();
 
+  @Test
+  public void test() {
+    logger.info("Start of executing main.testclasses.ExtClasses");
+
+    ClassRunner.runClass2("ExtClasses");
+
+    logger.info("Finished executing main.testclasses.ExtClasses");
+  }
 }
