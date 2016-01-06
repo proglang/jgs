ba6abefb618d196c3a0b30fdc1e56bef71479ea6
Regina Koenig <koenig_regina@arcor.de>
2016-01-06 19:27:43 +0100

    First illegal flow is found by running a class as a jUnit test

--- DynamicAnalyzer/tests/classfiletests/SimpleTest.java
+++ DynamicAnalyzer/tests/classfiletests/SimpleTest.java
@@ -1,8 +1,10 @@
 package classfiletests;
 
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import utils.exceptions.IllegalFlowException;
 import utils.logging.L1Logger;
 
 import java.util.logging.Logger;
@@ -16,9 +18,12 @@ public class SimpleTest{
   @Test
   public void test() {
     logger.info("Start of executing main.testclasses.Simple");
-
-    ClassRunner.runClass2("Simple");
-
+    try {
+      ClassRunner.runClass2("Simple");
+    } catch (Exception e) {
+      assertEquals(e.getCause().getClass(), IllegalFlowException.class);
+      logger.info("Successfully found an illegal flow");
+    }
     logger.info("Finished executing main.testclasses.Simple");
   }
 }
