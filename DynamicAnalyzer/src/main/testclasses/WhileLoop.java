a5e6c41af8e7b985258f263ad731186b395f1bb0
Regina Koenig <koenig_regina@arcor.de>
2016-03-17 19:26:14 +0100

    While-loop works.

--- DynamicAnalyzer/src/main/testclasses/WhileLoop.java
+++ DynamicAnalyzer/src/main/testclasses/WhileLoop.java
@@ -3,8 +3,19 @@ package main.testclasses;
 public class WhileLoop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		simpleWhile(2);
+	}
+	
+	/**
+	 * Simple while-loop.
+	 * @param x input
+	 * @return output
+	 */
+	public static int simpleWhile(int x) {
+		while (x < 10) {
+			x++;
+		}
+		return x;
 	}
 
 }
