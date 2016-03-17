f378fa0d6a1d53141e8f3301f6cd168a3acee58f
Regina Koenig <koenig_regina@arcor.de>
2016-03-17 19:33:07 +0100

    Switch statements work

--- DynamicAnalyzer/src/main/testclasses/SwitchStmt.java
+++ DynamicAnalyzer/src/main/testclasses/SwitchStmt.java
@@ -3,8 +3,27 @@ package main.testclasses;
 public class SwitchStmt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		simpleSwitch(3);
+	}
+	
+	/**
+	 * Simple switch-testcase.
+	 * @param x input
+	 * @return output
+	 */
+	public static int simpleSwitch(int x) {
+		switch (x) {
+		  case 1: 
+			  x++; 
+			  break;
+		  case 2: 
+			  x--; 
+			  break;
+		  default: 
+			  x = 100; 
+			  break;
+		}
+		return x;
 	}
 
 }
