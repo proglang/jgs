3c74dba2870960f5bd3cef9d93309c0bfca2fbb3
Regina Koenig <koenig_regina@arcor.de>
2016-03-17 19:05:01 +0100

    Testclass for for-loops. LengthExpr is unhandled.

--- DynamicAnalyzer/src/main/testclasses/ForLoop.java
+++ DynamicAnalyzer/src/main/testclasses/ForLoop.java
@@ -2,8 +2,13 @@ package main.testclasses;
 
 public class ForLoop {
 
+	/**
+	 * Test various for loops.
+	 * @param args Not used
+	 */
 	public static void main(String[] args) {
 		simpleFor(5);
+		arrayIterator(new String[]{"hg", "jh", "kj", "ef"});
 
 	}
 
@@ -17,5 +22,18 @@ public class ForLoop {
 			x--;
 		}
 		return x;
+	}
+	
+	/**
+	 * Test array iterator.
+	 * @param x input
+	 * @return output
+	 */
+	public static int arrayIterator(String[] x) {
+		int z = 0;
+		for (String y : x) {
+			z++;
+		}
+		return z;
 	}
 }
