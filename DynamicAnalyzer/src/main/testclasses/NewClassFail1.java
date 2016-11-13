db1210eafb0a219853813606fa509f62010438a7
Nico Müller <nicolasm@gmx.net>
2016-11-13 17:20:47 +0100

    add test: handeling classes

--- DynamicAnalyzer/src/main/testclasses/NewClassFail1.java
+++ DynamicAnalyzer/src/main/testclasses/NewClassFail1.java
@@ -2,13 +2,19 @@ package main.testclasses;
 
 import utils.analyzer.HelperClass;
 
-
-public class SimpleDebug {
+/**
+ * Testing explicit information leak of a class. Was not implemented until
+ * November 2016
+ * 
+ * @author Nicolas Müller
+ *
+ */
+public class NewClassFail1 {
 	public static void main(String[] args) {
-		
+
 		C b = new C();
 		C c = HelperClass.makeHigh(b);
 		System.out.println(c);
-		
+
 	}
 }
