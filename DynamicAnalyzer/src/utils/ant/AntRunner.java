8044292038706a67fa7703a6c9f30e0c1c754ce4
Nico Müller <nicolasm@gmx.net>
2016-11-21 18:00:14 +0100

    create via java

--- DynamicAnalyzer/src/utils/ant/AntRunner.java
+++ DynamicAnalyzer/src/utils/ant/AntRunner.java
@@ -1,10 +1,12 @@
 package utils.ant;
 
+import java.io.File;
+
 import org.apache.tools.ant.*;
 
 public class AntRunner {
-	public static void main() {
-		File buildFile = new File("build.xml");
+	public static void main(String[] args) {
+		File buildFile = new File("src/utils/ant/build.xml");
 		Project p = new Project();
 		p.setUserProperty("ant.file", buildFile.getAbsolutePath());		
 		DefaultLogger consoleLogger = new DefaultLogger();
@@ -12,5 +14,17 @@ public class AntRunner {
 		consoleLogger.setOutputPrintStream(System.out);
 		consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
 		p.addBuildListener(consoleLogger);
+
+		try {
+			p.fireBuildStarted();
+			p.init();
+			ProjectHelper helper = ProjectHelper.getProjectHelper();
+			p.addReference("ant.projectHelper", helper);
+			helper.parse(p, buildFile);
+			p.executeTarget(p.getDefaultTarget());
+			p.fireBuildFinished(null);
+		} catch (BuildException e) {
+			p.fireBuildFinished(e);
+		}
 	}
 }
