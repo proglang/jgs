709dec3ff50ab6b354ae6c832a9db76bcd869e07
Nico Müller <nicolasm@gmx.net>
2016-11-21 23:20:02 +0100

    BUG

--- DynamicAnalyzer/tests/end2endtest/SingleEndToEndTest.java
+++ DynamicAnalyzer/tests/end2endtest/SingleEndToEndTest.java
@@ -22,7 +22,7 @@ import java.util.logging.Logger;
  */
 public class SingleEndToEndTest {
 
-	public String name = "NSUPolicy3";
+	public String name = "AccessFieldsOfObjectsFail";
 
 	public boolean hasIllegalFlow = true;
 
