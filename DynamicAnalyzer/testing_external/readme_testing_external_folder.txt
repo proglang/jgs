409403243153003382fcc98e10f0f1fec8686bd2
Nicolas Müller <NicolasM@gmx.net>
2017-01-11 15:18:25 +0100

    little bit of cleanup

---
+++ DynamicAnalyzer/testing_external/readme_testing_external_folder.txt
@@ -0,0 +1,6 @@
+the folder testing_external contains java source.
+the folder exists only so that we can test the -p flag, which
+make our program look at another path for source files!
+So, this folder must be left here and not be tampered with!
+
+N. Müller, 10.01.2017
--- DynamicAnalyzer/tests/end2endtest/compileToJarTests.java
+++ DynamicAnalyzer/tests/end2endtest/compileToJarTests.java
@@ -3,161 +3,134 @@ package end2endtest;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Target;
 import org.apache.tools.ant.taskdefs.Java;
-import org.apache.tools.ant.types.Path;
 import org.junit.Test;
 import utils.exceptions.IllegalFlowException;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
-
 import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Scanner;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
 
 public class compileToJarTests {
-	
-	private String outputPath = "sootOutput/argsTest";
 
-	private static String convertStreamToString(java.io.InputStream is) {
-	    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-	    String ret = s.hasNext() ? s.next() : "";
-	    s.close();
-	    return ret;
-	}
-	
-	/**
-	 * NSUPolicy1 is present only on external_path, not in src/main...
-	 * NSUPolicy is present only in src/main, not on external_path...
-	 */
+    private String outputPath = "sootOutput/argsTest";
 
-	@Test
-	public void pathTest() {
-		String testFile = "NSUPolicy";
+    /**
+     * NSUPolicy1 is present only on external_path, not in src/main...
+     * NSUPolicy is present only in src/main, not on external_path...
+     */
 
-		main.Main.main(new String[] {"main.testclasses." + testFile, "-o", outputPath});
-		File outParent = new File(System.getProperty("user.dir"));
-		File outFile = new File(outParent, outputPath + "/main/testclasses/"+ testFile +".class");
-		File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
+    @Test
+    public void pathTest() {
+        String testFile = "NSUPolicy";
 
-		assertTrue(outFile.exists());
-		assertTrue(outJar.exists());
+        main.Main.main(new String[]{"main.testclasses." + testFile, "-o", outputPath});
+        File outParent = new File(System.getProperty("user.dir"));
+        File outFile = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".class");
+        File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
 
-		// delete for valid run next time
-		outFile.delete();
-		outJar.delete();
+        assertTrue(outFile.exists());
+        assertTrue(outJar.exists());
 
-		assertTrue(!outFile.exists());
-		assertTrue(!outJar.exists());
-	}
-	
-	@Test
-	public void pathTestJimple() {
-		String testFile = "NSUPolicy";
+        // delete for valid run next time
+        assertTrue(outFile.delete());
+        assertTrue(outJar.delete());
+    }
 
-		main.Main.main(new String[] {"main.testclasses." + testFile, "-o", outputPath, "-j"});
-		File outParent = new File(System.getProperty("user.dir"));
-		File outFile = new File(outParent, outputPath + "/main.testclasses."+ testFile +".jimple");
-		File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
+    @Test
+    public void pathTestJimple() {
+        String testFile = "NSUPolicy";
 
-		assertTrue(outFile.exists());
-		assertTrue(outJar.exists());
+        main.Main.main(new String[]{"main.testclasses." + testFile, "-o", outputPath, "-j"});
+        File outParent = new File(System.getProperty("user.dir"));
+        File outFile = new File(outParent, outputPath + "/main.testclasses." + testFile + ".jimple");
+        File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
 
-		// delete for valid run next time
-		outFile.delete();
-		outJar.delete();
+        assertTrue(outFile.exists());
+        assertTrue(outJar.exists());
 
-		assertTrue(!outFile.exists());
-		assertTrue(!outJar.exists());
-	}
-	
-	/**
-	 * Note that the externalPath must be set individually on each computer!
-	 */
-	@Test
-	public void pathTestP() {
-		String testFile = "NSUPolicy1";		// SHOULD BE NSUPolicy1 !! Not working right now
-		String externalPath = "/Users/NicolasM/Dropbox/hiwi/progLang/jgs/DynamicAnalyzer/testing_external";
+        // delete for valid run next time
+        assertTrue(outFile.delete());
+        assertTrue(outJar.delete());
+    }
 
-		main.Main.main(new String[] {"main.testclasses." + testFile, "-o", outputPath, "-p", externalPath});
-		File outParent = new File(System.getProperty("user.dir"));
-		File outFile = new File(outParent, outputPath + "/main/testclasses/"+ testFile +".class");
-		File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
+    /**
+     * Note that the externalPath must be set individually on each computer!
+     */
+    @Test
+    public void pathTestP() {
+        String testFile = "NSUPolicy1";        // SHOULD BE NSUPolicy1 !! Not working right now
+        String externalPath = "/Users/NicolasM/Dropbox/hiwi/progLang/jgs/DynamicAnalyzer/testing_external";
 
-		assertTrue(outFile.exists());
-		assertTrue(outJar.exists());
+        main.Main.main(new String[]{"main.testclasses." + testFile, "-o", outputPath, "-p", externalPath});
+        File outParent = new File(System.getProperty("user.dir"));
+        File outFile = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".class");
+        File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
 
-		// delete for valid run next time
-		outFile.delete();
-		outJar.delete();
+        assertTrue(outFile.exists());
+        assertTrue(outJar.exists());
 
-		assertTrue(!outFile.exists());
-		assertTrue(!outJar.exists());
-	}
-	
-	/**
-	 * NSUPolicy1 is present only on external_path, not in src/main...
-	 * NSUPolicy is present only in src/main, not on external_path...
-	 */
-	@Test
-	public void pathTestPRelative() {
-		String testFile = "NSUPolicy1";
-		String externalPath = "testing_external";
+        // delete for valid run next time
+        assertTrue(outFile.delete());
+        assertTrue(outJar.delete());
+    }
 
-		main.Main.main(new String[] {"main.testclasses." + testFile, "-o", outputPath, "-p", externalPath});
-		File outParent = new File(System.getProperty("user.dir"));
-		File outFile = new File(outParent, outputPath + "/main/testclasses/"+ testFile +".class");
-		File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
+    /**
+     * NSUPolicy1 is present only on external_path, not in src/main...
+     * NSUPolicy is present only in src/main, not on external_path...
+     */
+    @Test
+    public void pathTestPRelative() {
+        String testFile = "NSUPolicy1";
+        String externalPath = "testing_external";
 
-		assertTrue(outFile.exists());
-		assertTrue(outJar.exists());
+        main.Main.main(new String[]{"main.testclasses." + testFile, "-o", outputPath, "-p", externalPath});
+        File outParent = new File(System.getProperty("user.dir"));
+        File outFile = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".class");
+        File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
 
-		// delete for valid run next time
-		outFile.delete();
-		outJar.delete();
+        assertTrue(outFile.exists());
+        assertTrue(outJar.exists());
 
-		assertTrue(!outFile.exists());
-		assertTrue(!outJar.exists());
-	}
-	
+        // delete for valid run next time
+        assertTrue(outFile.delete());
+        assertTrue(outJar.delete());
+    }
 
-	@Test
+
+    @Test
     public void staticMethodsFail() {
-	    addFilesTest("StaticMethodsFail", "main.testclasses.utils.SimpleObject");
+        runAndTestJar("StaticMethodsFail", "main.testclasses.utils.SimpleObject", true);
     }
 
     @Test
     public void externalFail1() {
-	    addFilesTest("ExternalFail1", "main.testclasses.utils.simpleClassForTests");
+        runAndTestJar("ExternalFail1", "main.testclasses.utils.simpleClassForTests", true);
     }
 
     /**
-     * Super awesome test: Builds a jar with -f flag, instruments and includes external classes,
-     * builds to jar, runs it and tests for IllegalFlowException.
+     * Super awesome test: Takes a testfile, one additional file, and a boolean hasIllegalFLow.
+     * Build Jar, runs it and tests for illegalFlowException.
      */
-	private void addFilesTest(String testFile, String additionalFile) {
-		boolean hasIllegalFlow = true;
+    private void runAndTestJar(String testFile, String additionalFile, boolean hasIllegalFlow) {
         final String TARGET_NAME = "exec";
 
-		String externalPath = "testing_external";
+        String externalPath = "testing_external";
 
-		main.Main.main(new String[] {"main.testclasses." + testFile, "-o", outputPath, "-p", externalPath, "-f", additionalFile});
-		File outParent = new File(System.getProperty("user.dir"));
-		File outFile = new File(outParent, outputPath + "/main/testclasses/"+ testFile +".class");
-		File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
-		File addFile = new File(outParent, outputPath + "/" + additionalFile.replace(".", "/") + ".class");
+        main.Main.main(new String[]{"main.testclasses." + testFile, "-o", outputPath, "-p", externalPath, "-f", additionalFile});
+        File outParent = new File(System.getProperty("user.dir"));
+        File outFile = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".class");
+        File outJar = new File(outParent, outputPath + "/main/testclasses/" + testFile + ".jar");
+        File addFile = new File(outParent, outputPath + "/" + additionalFile.replace(".", "/") + ".class");
 
-		assertTrue(outFile.exists());
-		assertTrue(outJar.exists());
-		assertTrue(addFile.exists());
+        assertTrue(outFile.exists());
+        assertTrue(outJar.exists());
+        assertTrue(addFile.exists());
 
-		// now run it and check wheter or not it produces an IllegalFlowException
-		System.out.println("Running " + outJar.toString() + "...");
+        // now run it and check wheter or not it produces an IllegalFlowException
+        System.out.println("Running " + outJar.toString() + "...");
         File out = new File(outputPath + "/outStreamFile");
 
         try {
@@ -184,6 +157,7 @@ public class compileToJarTests {
 
             target.execute();
             project.executeTarget(project.getDefaultTarget());
+
         } catch (IllegalFlowException e) {
             System.out.println("IllegalFlowException!! : " + e);
         } catch (Exception e) {
@@ -193,9 +167,13 @@ public class compileToJarTests {
             // just to get it working.
             // =======================
             try {
-                Scanner scanner = new Scanner( out );
+                Scanner scanner = new Scanner(out);
                 String text = scanner.useDelimiter("\\A").next();
-                assertThat(text, containsString("IllegalFlowException"));
+                if (hasIllegalFlow) {
+                    assertThat(text, containsString("IllegalFlowException"));
+                } else {
+                    assertThat(text, not(containsString("IllegalFlowException")));
+                }
                 scanner.close(); // Put this call in a finally block
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -210,6 +188,6 @@ public class compileToJarTests {
         assertTrue(outJar.delete());
         assertTrue(addFile.delete());
 
-	}
-	
+    }
+
 }
