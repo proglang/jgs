11664d080584468f81fdd1854846169eb89e6ee6
Nicolas Müller <NicolasM@gmx.net>
2017-03-14 17:40:15 +0100

    JGS: add SimpleFail

--- src/main/java/testclasses/SimpleFail.java
+++ src/main/java/testclasses/SimpleFail.java
@@ -16,27 +16,6 @@ public class SimpleFail {
         String hello = (aStaticMethodWithCasts(hello1)); // fails at run-time due to an bad cast in aStaticMethodWithCasts
     }
 
-    int answer = 42;
-
-    @Sec("HIGH")
-    int high = 123;
-
-    @Sec("HIGH")
-    Object highObject = new Object();
-
-
-    static int aStatic = 0;
-
-    @Constraints({"LOW <= @0 ", "@0 <= @ret"})
-    @Effects({})
-    static int aStaticMethod(int i) {
-        if (i == 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
     @Constraints({"HIGH <= @0 ", "@0 <= @ret"})
     @Effects({})
     static String aStaticMethodWithCasts(String i) {
@@ -48,15 +27,4 @@ public class SimpleFail {
         }
     }
 
-    @Constraints({"pub <= @0", "? <= @1", "@0 <= @ret", "@1 <= @ret"})
-    public int methodWithImplicitFlows(boolean i, boolean j) {
-        int z = 0;
-        if (i) {
-            z += 10;
-        }
-        if (j) {
-            z += 1;
-        }
-        return z;
-    }
 }
