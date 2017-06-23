51e7b2b9e2bcd95f51b868321dccad5f981b869f
Luminous Fennell <fennell@informatik.uni-freiburg.de>
2017-06-23 14:39:40 +0200

    Add color output for type errors

--- DynamicAnalyzer/src/main/java/utils/logging/L1Formatter.java
+++ DynamicAnalyzer/src/main/java/utils/logging/L1Formatter.java
@@ -1,15 +1,26 @@
 package utils.logging;
 
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class L1Formatter extends Formatter{
-
+	// TODO: ansi colors are just a demo hack, here
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_BLACK = "\u001B[30m";
+public static final String ANSI_RED = "\u001B[31m";
 	@Override
 	public String format(LogRecord rec) {
 		StringBuffer buf = new StringBuffer(1000);
 
+		if ( rec.getLevel() == Level.SEVERE) {
+			buf.append(ANSI_RED);
+		}
+
 		buf.append(formatMessage(rec));
+		if ( rec.getLevel() == Level.SEVERE) {
+			buf.append(ANSI_RESET);
+		}
 		buf.append("\n\t\t\t\t\t (");
 		buf.append(rec.getLevel());
 		buf.append(" - ");
